d94ee75caaa82c5cfce3f5b118e3c93cab305eb1
dhanashreebagalqnopy <dhanashre.bagal@qnopy.com>
2026-08-01 14:56:23 +0530

    chore: update base URL to /portfolio/ in vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,7 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/dhanashree-bagal-portfolio/',
+  base: '/portfolio/',
   server: {
     port: 3000,
     open: true
